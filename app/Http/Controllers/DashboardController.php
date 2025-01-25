1bdefd884d38ed35593108ae8095fc5b1d52b670
Farul Wananda <farulahmad2002@gmail.com>
2025-01-25 21:41:47 +0700

    feat: update dashboard to display user, masyarakat, kriteria, and sub kriteria counts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Kriteria;
+use App\Models\Masyarakat;
+use App\Models\SubKriteria;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,6 +15,11 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('pages.dashboard.index');
+        $dataUsers = User::count();
+        $dataMasyarakats = Masyarakat::count();
+        $dataKriterias = Kriteria::count();
+        $dataSubKriterias = SubKriteria::count();
+
+        return view('pages.dashboard.index', compact('dataUsers', 'dataMasyarakats', 'dataKriterias', 'dataSubKriterias'));
     }
 }
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -16,11 +16,10 @@
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="mr-2 col">
-                                <div class="mb-1 text-xs font-weight-bold text-uppercase">Jumlah Calon Penerima</div>
-                                <div class="mb-0 text-gray-800 h5 font-weight-bold">$40,000</div>
+                                <div class="mb-1 text-xs font-weight-bold text-uppercase">Jumlah Masyarakat</div>
+                                <div class="mb-0 text-gray-800 h5 font-weight-bold">{{ $dataMasyarakats }}</div>
                                 <div class="mt-2 mb-0 text-xs text-muted">
-                                    <span class="mr-2 text-success"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                                    <span>Since last month</span>
+                                    <span>Msayarakat Yang Terdaftar</span>
                                 </div>
                             </div>
                             <div class="col-auto">
@@ -30,17 +29,15 @@
                     </div>
                 </div>
             </div>
-            <!-- Earnings (Annual) Card Example -->
             <div class="mb-4 col-xl-3 col-md-6">
                 <div class="card h-100">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="mr-2 col">
-                                <div class="mb-1 text-xs font-weight-bold text-uppercase">Penerima Bantuan</div>
-                                <div class="mb-0 text-gray-800 h5 font-weight-bold">650</div>
+                                <div class="mb-1 text-xs font-weight-bold text-uppercase">Jumlah Kriteria</div>
+                                <div class="mb-0 text-gray-800 h5 font-weight-bold">{{ $dataKriterias }}</div>
                                 <div class="mt-2 mb-0 text-xs text-muted">
-                                    <span class="mr-2 text-success"><i class="fas fa-arrow-up"></i> 12%</span>
-                                    <span>Since last years</span>
+                                    <span>Kriteria Yang Terdaftar</span>
                                 </div>
                             </div>
                             <div class="col-auto">
@@ -50,17 +47,15 @@
                     </div>
                 </div>
             </div>
-            <!-- New User Card Example -->
             <div class="mb-4 col-xl-3 col-md-6">
                 <div class="card h-100">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="mr-2 col">
-                                <div class="mb-1 text-xs font-weight-bold text-uppercase">Jumlah User</div>
-                                <div class="mb-0 mr-3 text-gray-800 h5 font-weight-bold">366</div>
+                                <div class="mb-1 text-xs font-weight-bold text-uppercase">Jumlah Sub Kriteria</div>
+                                <div class="mb-0 mr-3 text-gray-800 h5 font-weight-bold">{{ $dataSubKriterias }}</div>
                                 <div class="mt-2 mb-0 text-xs text-muted">
-                                    <span class="mr-2 text-success"><i class="fas fa-arrow-up"></i> 20.4%</span>
-                                    <span>Since last month</span>
+                                    <span>Sub Kriteria Yang Terdaftar</span>
                                 </div>
                             </div>
                             <div class="col-auto">
@@ -70,17 +65,15 @@
                     </div>
                 </div>
             </div>
-            <!-- Pending Requests Card Example -->
             <div class="mb-4 col-xl-3 col-md-6">
                 <div class="card h-100">
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="mr-2 col">
-                                <div class="mb-1 text-xs font-weight-bold text-uppercase">Pending Requests</div>
-                                <div class="mb-0 text-gray-800 h5 font-weight-bold">18</div>
+                                <div class="mb-1 text-xs font-weight-bold text-uppercase">Jumlah Pengguna</div>
+                                <div class="mb-0 text-gray-800 h5 font-weight-bold">{{ $dataUsers }}</div>
                                 <div class="mt-2 mb-0 text-xs text-muted">
-                                    <span class="mr-2 text-danger"><i class="fas fa-arrow-down"></i> 1.10%</span>
-                                    <span>Since yesterday</span>
+                                    <span>Pengguna Yang Terdaftar</span>
                                 </div>
                             </div>
                             <div class="col-auto">
@@ -88,95 +81,6 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-
-            <!-- Area Chart -->
-            <div class="col-xl-12 col-lg-12">
-                <div class="mb-4 card">
-                    <div class="flex-row py-3 card-header d-flex align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Persentase Penerima Bantuan</h6>
-                        <div class="dropdown no-arrow">
-                            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="text-gray-400 fas fa-ellipsis-v fa-sm fa-fw"></i>
-                            </a>
-                            <div class="shadow dropdown-menu dropdown-menu-right animated--fade-in"
-                                aria-labelledby="dropdownMenuLink">
-                                <div class="dropdown-header">Dropdown Header:</div>
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="chart-area">
-                            <canvas id="myAreaChart"></canvas>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Invoice Example -->
-            <div class="mb-4 col-xl-12 col-lg-12">
-                <div class="card">
-                    <div class="flex-row py-3 card-header d-flex align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Invoice</h6>
-                        <a class="float-right m-0 btn btn-danger btn-sm" href="#">View More <i
-                                class="fas fa-chevron-right"></i></a>
-                    </div>
-                    <div class="table-responsive">
-                        <table class="table align-items-center table-flush">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th>Order ID</th>
-                                    <th>Customer</th>
-                                    <th>Item</th>
-                                    <th>Status</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td><a href="#">RA0449</a></td>
-                                    <td>Udin Wayang</td>
-                                    <td>Nasi Padang</td>
-                                    <td><span class="badge badge-success">Delivered</span></td>
-                                    <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href="#">RA5324</a></td>
-                                    <td>Jaenab Bajigur</td>
-                                    <td>Gundam 90' Edition</td>
-                                    <td><span class="badge badge-warning">Shipping</span></td>
-                                    <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href="#">RA8568</a></td>
-                                    <td>Rivat Mahesa</td>
-                                    <td>Oblong T-Shirt</td>
-                                    <td><span class="badge badge-danger">Pending</span></td>
-                                    <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href="#">RA1453</a></td>
-                                    <td>Indri Junanda</td>
-                                    <td>Hat Rounded</td>
-                                    <td><span class="badge badge-info">Processing</span></td>
-                                    <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
-                                </tr>
-                                <tr>
-                                    <td><a href="#">RA1998</a></td>
-                                    <td>Udin Cilok</td>
-                                    <td>Baby Powder</td>
-                                    <td><span class="badge badge-success">Delivered</span></td>
-                                    <td><a href="#" class="btn btn-sm btn-primary">Detail</a></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="card-footer"></div>
                 </div>
             </div>
         </div>
